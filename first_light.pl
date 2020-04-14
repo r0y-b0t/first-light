46269093ecf24773f25db8737d0793c6e92263c3
R3DL1N3 <r0y@digitalcombatsimulator.world>
2020-04-14 17:12:19 +0000

    Description and tasks

---
+++ first_light.pl
@@ -0,0 +1,21 @@
+﻿hook(description("The time will come, when thou shalt lift thine eyes\n\c
+To watch a long-drawn battle in the skies.\n\c
+While aged peasants, too amazed for words,\n\c
+Stare at the flying fleets of wond’rous birds.\n\c
+\n\c
+England, so long mistress of the sea,\n\c
+Where winds and waves confess her sovereignty\n\c
+Her ancient triumphs yet on high shall bear,\n\c
+And reign, the sovereign of the conquered air.\n\c
+\n\c
+Dedicated to the memory of Squadron Leader Geoffrey Wellum DFC, 1921-2018, and to all the pilots who fought in the Battle of Britain on both sides.\n\c
+\n\c
+(WIP)")).
+
+hook(task(red, "The English are wondering. They keep asking, “Why doesn’t he come?” Be patient. Patience. We are coming. We are coming! Sieg heil! Sieg heil!")).
+
+hook(task(blue, "Spring chicken to shite hawk in one easy lesson. Look! Search for the bastards! And never fly in a straight line, or you’re a dead duck. Or, in other words, beware of the Hun in the sun.\n\c
+\n\c
+Never was so much owed to so few.")).
+
+hook(task(neutrals, "")).
